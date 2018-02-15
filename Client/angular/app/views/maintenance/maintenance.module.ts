4d70bc290101d5e0acd898b3416a9b9a6457a8eb
Eros Niko Cas Alvarez <iamerosniko@yahoo.com>
2018-02-15 13:52:32 -0500

    created components
    Search
    Maintenance
    Main page
    Reports

---
+++ Client/angular/app/views/maintenance/maintenance.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MaintenanceComponent } from './maintenance.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [MaintenanceComponent]
+})
+export class MaintenanceModule { }
